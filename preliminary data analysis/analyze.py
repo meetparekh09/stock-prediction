5c194fcfc4e77aa029ea1ee4d3a44db43b9f5727
meetparekh09 <meetparekh09@gmail.com>
2018-06-19 16:47:51 -0400

    Preliminary data analysis commit 3: writing headline, body and pub date to a single text file

--- preliminary data analysis/analyze.py
+++ preliminary data analysis/analyze.py
@@ -5,10 +5,13 @@ microsoftHeadlineCount = 0
 associatedHeadlineCount = 0
 yearCount = {}
 microsoftYearCount = {}
+writeFile = open('./merged-file.txt', 'w')
 
-for filename in os.listdir("../../Microsoft/The Wall Street Journal"):
+filelist = os.listdir("../../Microsoft/The Wall Street Journal")
+filelist.sort()
+for filename in filelist:
     if filename.endswith(".txt"):
-        # print(os.path.join("../../Microsoft/The Wall Street Journal", filename))
+        print(os.path.join("../../Microsoft/The Wall Street Journal", filename))
         filepath = os.path.join("../../Microsoft/The Wall Street Journal", filename)
 
         startOfArticle = False
@@ -18,7 +21,9 @@ for filename in os.listdir("../../Microsoft/The Wall Street Journal"):
         headline = ''
         body = ''
         date = ''
+
         with open(filepath) as file:
+            # file.readline()
             for line in file:
                 if line == '____________________________________________________________\n':
                     startOfArticle = not startOfArticle
@@ -27,6 +32,10 @@ for filename in os.listdir("../../Microsoft/The Wall Street Journal"):
                     continue
                 if endOfArticle:
                     count += 1
+                    writeFile.write(headline)
+                    writeFile.write(date)
+                    writeFile.write(body)
+                    writeFile.write('\n\n')
                     # print(headline)
                     if 'Microsoft' in headline:
                         microsoftHeadlineCount += 1
@@ -41,7 +50,7 @@ for filename in os.listdir("../../Microsoft/The Wall Street Journal"):
                                 microsoftYearCount[year] = 1
                             # print(year)
                     # print(body)
-                    print(date)
+                    # print(date)
 
                     if date != '':
                         year = int(date[-6:].replace(" ", ""))
@@ -49,7 +58,7 @@ for filename in os.listdir("../../Microsoft/The Wall Street Journal"):
                             yearCount[year] += 1
                         else:
                             yearCount[year] = 1
-                        print(year)
+                        # print(year)
 
                     headline = ''
                     body = ''
@@ -65,12 +74,12 @@ for filename in os.listdir("../../Microsoft/The Wall Street Journal"):
 
                     continue
 
-                if startOfArticle and line != '\n' and headline == '':
+                if startOfArticle and line != '\n' and line != '____________________________________________________________\n' and headline == '':
                     headline = line
                 if startOfArticle and line.startswith('Full text:'):
                     bodyStart = True
                     bodyEnd = False
-                if startOfArticle and (line.startswith('Credit:') or line.startswith('Title:')):
+                if startOfArticle and (line.startswith('Credit:') or line.startswith('Title:')) or line.startswith('Subject:'):
                      bodyStart = False
                      bodyEnd = True
 
@@ -86,3 +95,84 @@ print(associatedHeadlineCount)
 
 print(yearCount)
 print(microsoftYearCount)
+writeFile.close()
+# count = 0
+# microsoftHeadlineCount = 0
+# associatedHeadlineCount = 0
+# yearCount = {}
+# microsoftYearCount = {}
+#
+# for filename in os.listdir("../../Microsoft/The Wall Street Journal Factiva"):
+#     if filename.endswith(".txt"):
+#         # print(os.path.join("../../Microsoft/The Wall Street Journal", filename))
+#         filepath = os.path.join("../../Microsoft/The Wall Street Journal Factiva", filename)
+#
+#         startOfArticle = False
+#         bodyStart = False
+#         bodyEnd = True
+#         endOfArticle = True
+#         headline = ''
+#         body = ''
+#         date = ''
+#
+#         with open(filepath) as file:
+#             for line in file:
+#                 if line == '____________________________________________________________\n':
+#                     startOfArticle = not startOfArticle
+#                     endOfArticle = not endOfArticle
+#                 if line == '\n':
+#                     continue
+#                 if endOfArticle:
+#                     count += 1
+#                     # print(headline)
+#                     if 'Microsoft' in headline:
+#                         microsoftHeadlineCount += 1
+#
+#                     if 'Microsoft' in headline or 'Skype' in headline or 'Xbox' in headline or 'Outlook' in headline or 'Windows' in headline:
+#                         associatedHeadlineCount += 1
+#                         if date != '':
+#                             year = int(date[-6:].replace(" ", ""))
+#                             if year in microsoftYearCount.keys():
+#                                 microsoftYearCount[year] += 1
+#                             else:
+#                                 microsoftYearCount[year] = 1
+#                             # print(year)
+#                     # print(body)
+#                     print(date)
+#
+#                     if date != '':
+#                         year = int(date[-6:].replace(" ", ""))
+#                         if year in yearCount.keys():
+#                             yearCount[year] += 1
+#                         else:
+#                             yearCount[year] = 1
+#                         print(year)
+#
+#                     headline = ''
+#                     body = ''
+#                     date = ''
+#                     endOfArticle = False
+#                     startOfArticle = True
+#
+#                     # try:
+#                     #     year = int(s.split(',')[-1].replace(" ", ""))
+#                     #     print(year)
+#                     # except:
+#                     #     continue
+#
+#                     continue
+#
+#                 if startOfArticle and line != '\n' and headline == '':
+#                     headline = line
+#                 if startOfArticle and line.startswith('Full text:'):
+#                     bodyStart = True
+#                     bodyEnd = False
+#                 if startOfArticle and (line.startswith('Credit:') or line.startswith('Title:')):
+#                      bodyStart = False
+#                      bodyEnd = True
+#
+#                 if startOfArticle and line.startswith('Publication date:'):
+#                     date = line
+#
+#                 if startOfArticle and bodyStart:
+#                     body += line
